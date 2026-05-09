0658792f290e2c001f2632113c89696eb5a8fde8
tannguyen888 <triumphdash00@gmai.com>
2026-05-08 21:41:05 -0400

    add reuse custom scss btn, with props, color clone from tiktok

--- src/components/GlobalStyle/Layout/components/Header/header.js
+++ src/components/GlobalStyle/Layout/components/Header/header.js
@@ -1,12 +1,25 @@
 import HeaderStyles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import img from '~/assets/img';
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Button from '../button';
 
 const cx = classNames.bind(HeaderStyles);
 
 function Header () {
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1,2,3]);
+        }, 3000);
+    }, []);
+
+
     return (
 
         <div >
@@ -14,15 +27,33 @@ function Header () {
             <div className={cx('inner')}>
                 <div className={cx('logo')}><img src={img.logo} alt="TikTok Logo" /></div>
                 <div className={cx('search')}>
+                     <Tippy
+                     visible={searchResult.length > 0}
+                      render={attr => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attr}>
+                           ket qua
+                        </div>
+                    )
+
+                    }> 
                     <input placeholder='Search Acounts and Videos' spellCheck={false}/>
+                    </Tippy>
                     <button className={cx('clear')}>
                      <FontAwesomeIcon icon={faCircleXmark} />
                         {/* loading*/}
                     </button>
                     <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                      <button className={cx('search-btn')}>
+                   
+                        <button className={cx('search-btn')}>
                        <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
+                   
+                     
+                    </div>
+                    <div className={cx('action')}>
+                         <Button text>Upload</Button>
+                        <Button primary leftIcon={ <FontAwesomeIcon icon={faSignIn} />} >Log In</Button>
+                       
                     </div>
             </div>
            </h2>
